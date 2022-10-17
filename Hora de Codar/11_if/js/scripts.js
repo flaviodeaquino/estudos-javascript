1c90645be1bea620edd865eb758f188dd13350a9
flaviodeaquino <flavio.deaquinof@gmail.com>
2022-10-17 18:31:01 -0300

    att estudo if else if

---
+++ Hora de Codar/11_if/js/scripts.js
@@ -0,0 +1,14 @@
+var idade = 17;
+var idadeMinima = 18;
+
+console.log("Antes do if");
+
+if(idade >= idadeMinima) {
+    console.log("Pode tirar a carteira de habilitação!");
+} else if (idade < idadeMinima ){
+    let anos = 0;
+    anos = idadeMinima - idade;
+    console.log(`Ainda faltam ${anos} anos para tirar a carteira.`);
+}
+
+console.log("Depois do if");
