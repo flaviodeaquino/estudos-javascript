bbd4abbf3358cd6d2728f436f9d1ea72f93b2179
flaviodeaquino <flavio.deaquinof@gmail.com>
2022-10-17 16:15:53 -0300

    att estudo array

---
+++ Hora de Codar/10_arrays/js/scripts.js
@@ -0,0 +1,17 @@
+var arr = [5, "Flávio", true, {teste: 1, teste:2}]; // obj => {}
+
+console.log(arr);
+
+var arr2 = [2, 3, 4, 5, 6];
+
+console.log(arr2);
+
+console.log(arr[1]);
+console.log(arr2[0]);
+
+arr[4] = [10]; // adicionar elemento no array
+
+arr[0] = "teste";
+
+console.log(arr);
+console.log(typeof arr);
